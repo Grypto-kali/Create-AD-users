0d2ce6e72e1adc873b4c0bab4606b36b504bcddd
gryptokali <96015863+Grypto-kali@users.noreply.github.com>
2023-11-22 11:37:42 +0200

    Update create_users.ps1

--- AD Make users/create_users.ps1
+++ AD Make users/create_users.ps1
@@ -1,29 +1,31 @@
 # Define the file path for the CSV file
 $CSVPath = "C:\Skriptit\userlist.csv"
 $GroupName = "Testi"  # Group name
-$OUPath = "OU=Testi,OU=OU - Kaikki,DC=te,DC=local"  # OU path
-$DomainPath = "te.local"
+$OUPath = "OU=Testi,OU=OU - Kaikki,DC=jm,DC=local"  # OU path
+$DomainPath = "jm.local"
 
 $Users = Import-Csv $CSVPath # Read the CSV file and create users
 
 foreach ($User in $Users) {
     $Username = $User.Username
-    $Password = "basswordXD"
+    $Password = "Kpedu@malessa"
     $Firstname = $User.Firstname
     $Lastname = $User.Lastname
     $OU = $OUPath  # OU path
     $Group = $GroupName  # Group name
+    $DisplayName = "$Firstname $Lastname"
+    $Description = $GroupName
 
     # Create a new user
     New-ADUser -SamAccountName $Username `
                 -UserPrincipalName "$Username@$DomainPath" `
-                -Name "$Firstname $Lastname" `
+                -Name "$DisplayName" `
                 -GivenName $Firstname `
                 -Surname $Lastname `
                 -Enabled $true `
                 -ChangePasswordAtLogon $false `
                 -PasswordNeverExpires $true `
-                -OtherAttributes @{'Description'='Testi'} `
+                -OtherAttributes @{'Description'=$Description} `
                 -Path $OU `
                 -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force)
 
